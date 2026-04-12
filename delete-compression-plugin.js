88f65a93c775804f8fd93b9b3241f1b887b677fb
LukeLee1188 <lhlee@wpi.edu>
2026-04-12 14:38:30 -0400

    Targeting app-level build.gradle for compression fix

--- delete-compression-plugin.js
+++ delete-compression-plugin.js
@@ -1,13 +1,16 @@
 const { withAppBuildGradle } = require('@expo/config-plugins');
 
-module.exports = function withRemoveCompression(config) {
+module.exports = function withKotlinFix(config) {
   return withAppBuildGradle(config, (config) => {
-    if (config.modResults.contents.includes('enableBundleCompression')) {
-      config.modResults.contents = config.modResults.contents.replace(
-        /enableBundleCompression\s*=\s*(true|false)/g,
-        '// Line removed by plugin'
-      );
+    let contents = config.modResults.contents;
+
+    // This targets the exact property causing the "unknown property" crash
+    if (contents.includes('enableBundleCompression')) {
+      console.log("Found the ghost property! Deleting from app/build.gradle...");
+      contents = contents.replace(/enableBundleCompression\s*=\s*(true|false)/g, '// Removed for RN 0.76');
     }
+
+    config.modResults.contents = contents;
     return config;
   });
 };
