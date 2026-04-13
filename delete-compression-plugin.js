0da4de093659caa24f557f089798bf25188b2286
LukeLee1188 <lhlee@wpi.edu>
2026-04-13 00:00:56 -0400

    Added config plugin to fix Line 17 compression error

--- delete-compression-plugin.js
+++ delete-compression-plugin.js
@@ -3,9 +3,10 @@ const { withAppBuildGradle } = require('@expo/config-plugins');
 module.exports = function withRemoveCompression(config) {
   return withAppBuildGradle(config, (config) => {
     if (config.modResults.contents.includes('enableBundleCompression')) {
+      // This deletes the exact line causing the Line 17 crash
       config.modResults.contents = config.modResults.contents.replace(
         /enableBundleCompression\s*=\s*.*/g,
-        '// Line removed by Fitly Plugin'
+        '// Line removed for RN 0.75 compatibility'
       );
     }
     return config;
